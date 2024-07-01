d54af91102691283b2d8f97847c601bef40d3136
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2024-07-01 21:53:38 +0100

    Setup multiprocessing logic

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from data_initializer import ensure_data_exists
 from helpers import load_words, load_colouring
 import os
+import multiprocessing
+
 
 if __name__ == "__main__":
     ensure_data_exists()
@@ -9,7 +11,10 @@ if __name__ == "__main__":
     GUESSES_PATH = os.path.join(DATA_DIRECTORY, 'allowed_guesses.txt')
     PRECOMPUTE_PATH = os.path.join(DATA_DIRECTORY, 'precompute.json')
 
+    state = ["setup", "smite", "smote", "spite", "stein", "stern", "stoke", "stone", "store", "stove", "suite"]
     guesses = load_words(GUESSES_PATH)
     precomputed_colouring = load_colouring(PRECOMPUTE_PATH)
-    
-    
+
+    manager = multiprocessing.Manager()
+    shared_guessed = manager.list(guesses)
+    shared_precomputed_colouring = manager.dict(precomputed_colouring)

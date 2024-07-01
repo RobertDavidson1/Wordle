e6f08dbf4d45e25761f8df4e90e898dd750b05c7
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2024-07-01 21:35:18 +0100

    Added emojis for better user interaction

--- src/data_initializer.py
+++ src/data_initializer.py
@@ -5,19 +5,19 @@ import time
 
 def download_file_if_missing(url, path):
     if not os.path.isfile(path):
-        print(f"{os.path.basename(path)} does not exist.")
-        print("Downloading..")
+        print(f"\n{os.path.basename(path)} does not exist. ❌")
+        print("Downloading.. ⏳")
 
         response = requests.get(url, verify=True)
         if response.status_code == 200:
             with open(path + '.tmp', 'w') as file:
                 file.write(response.text)
             os.replace(path + '.tmp', path)
-            print(f"Downloaded file to {path}\n")
+            print(f"Downloaded file. ✅")
         else:
             print(f"Failed to download {url}\n")
     else:
-        print(f"{os.path.basename(path)} exists")
+        print(f"{os.path.basename(path)} exists. ✅")
     
 
 
@@ -31,12 +31,12 @@ def ensure_data_exists() -> NoReturn:
     ANSWERS_URL = "https://raw.githubusercontent.com/RobertDavidson1/Wordle/main/data/allowed_answers.txt"
     
     if not os.path.isdir(DATA_DIRECTORY):
-        print("Data folder does not exist.")
-        print("Creating..")
+        print("Data folder does not exist. ❌")
+        print("Creating.. ⏳")
         os.makedirs(DATA_DIRECTORY)
-        print("Data folder created\n")
+        print("Data folder created. ✅\n")
     else:
-        print("Data folder exists")
+        print("Data folder exists. ✅")
     time.sleep(1)
     
     download_file_if_missing(GUESSES_URL, GUESSES_PATH)
@@ -46,9 +46,9 @@ def ensure_data_exists() -> NoReturn:
     time.sleep(1)
 
     if not os.path.isfile(PRECOMPUTE_PATH):
-        print("\nprecompute.json does not exist")
+        print("\nprecompute.json does not exist ❌")
         if os.path.isfile(os.path.join(os.path.dirname(__file__), 'precompute.py')):
-            print("Creating precompute.json..")
+            print("Creating precompute.json.. ⏳")
             time.sleep(3)
             from precompute import create_precompute_json
             create_precompute_json(DATA_DIRECTORY)
@@ -56,7 +56,7 @@ def ensure_data_exists() -> NoReturn:
             print("precompute.py is missing.")
             print("Please redownload from github")
     else:
-        print("precompute.json exists")
+        print("precompute.json exists. ✅")
         time.sleep(1)
         
 

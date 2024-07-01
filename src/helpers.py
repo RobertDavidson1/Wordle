371bed99ebef703377facf09cce9d8b952afef3f
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2024-07-01 21:36:06 +0100

    Fixed problem with loading and returning json

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import json
 
 def clear_terminal():
     current_os = platform.system()
@@ -12,3 +13,10 @@ def load_words(file_path):
     with open(file_path, "r") as file_handle:
         word_array = [word.strip() for word in file_handle]
     return word_array
+
+def load_colouring(file_path):
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    precompute_full_path = os.path.join(base_path, file_path)
+    with open(precompute_full_path, "r") as file:
+        data = json.load(file)
+    return data

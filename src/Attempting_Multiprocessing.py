2962e5619519a49945b60a5f1bd7995859e66c6b
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2024-07-03 17:12:25 +0100

    Cleanup code

--- src/Attempting_Multiprocessing.py
+++ src/Attempting_Multiprocessing.py
@@ -1,22 +1,15 @@
 import multiprocessing
-from helpers import load_words, load_colouring, clear_terminal, heuristic_split_and_insert
+from helpers import load_words, load_colouring, heuristic_split_and_insert
 import os
 
-
 def recursive_task(individual_list, shared_list, colouring_data, level=0):
-    if level > 2:
-        return
+    if level > 6: return
+    if level == 0: current_list = individual_list
+    else: current_list = shared_list
 
-    if level == 0:
-        current_list = individual_list
-    else:
-        current_list = shared_list
-
-    # Debugging information
     print(f"Process {multiprocessing.current_process().name} at level {level} using list {current_list[:5]}")
     print(f"Colouring {colouring_data['salet']['aback']}")
 
-    # Recursive call
     recursive_task(shared_list, shared_list, colouring_data, level + 1)
 
 
@@ -31,26 +24,13 @@ if __name__ == "__main__":
     PRECOMPUTE_PATH = os.path.join(DATA_DIRECTORY, 'precompute.json')
     
     colouring_data = load_colouring(PRECOMPUTE_PATH)
-    guesses = load_words(GUESSES_PATH)
+    shared_list = load_words(GUESSES_PATH)
     state = ('salet', 'trace')
-
-    # Create a manager to handle shared data
-    manager = multiprocessing.Manager()
+    indiv_list = heuristic_split_and_insert(shared_list, state, PROCESSES)
     
-    # Create a shared list and a shared dictionary
-    shared_list = guesses
+    processes = [multiprocessing.Process(target=process_task, args=(indiv_list[i], shared_list, colouring_data)) for i in range(PROCESSES)]
     
-    indiv_list = heuristic_split_and_insert(guesses,state,PROCESSES)
-
-    # Create a list to hold references to process objects
-    processes = []
-    
-    # Launch multiple processes
-    for i in range(PROCESSES):  # Let's create 5 processes
-        p = multiprocessing.Process(target=process_task, args=(indiv_list[i],shared_list, colouring_data))
-        processes.append(p)
+    for p in processes:
         p.start()
     
-    
-    # No need to wait for child processes to complete
     print("Main process does not wait for child processes to finish.")

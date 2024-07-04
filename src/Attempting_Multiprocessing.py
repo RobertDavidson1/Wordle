5bc672ca35f79e8ba12bccdd6138baf8bc63dfad
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2024-07-04 15:53:59 +0100

    Refactor and remove unique process for state

--- src/Attempting_Multiprocessing.py
+++ src/Attempting_Multiprocessing.py
@@ -3,8 +3,6 @@ from data_initializer import ensure_data_exists
 from helpers import load_words, load_colouring, heuristic
 import os
 
-
-
 def get_transition_info(state, action, colouring_data):
     new_state = {}
     for word in state:
@@ -17,7 +15,7 @@ def get_transition_info(state, action, colouring_data):
     return transition_info
 
 
-def compute_value(state, all_guesses, colouring_data, proccessUnique_list, shared_state, hash, depth=0, v_mem={}):
+def compute_value(state, all_guesses, colouring_data, filtered_split_actions, shared_state, hash, depth=0, v_mem={}):
     if v_mem.get((depth, state)):return v_mem[(depth, state)]
     # Exit cases
     if depth == 6 or (depth == 5 and len(state) > 1): return (float("inf"), None)
@@ -26,14 +24,13 @@ def compute_value(state, all_guesses, colouring_data, proccessUnique_list, share
     if len(state) == 2: return (1.5, (state)[0])
 
     if depth == 0: 
-        current_list = proccessUnique_list
+        current_list = filtered_split_actions
     else: 
         if state not in hash.keys():
             current_list = heuristic(all_guesses, state, colouring_data)
             hash[state] = current_list
         else:
             current_list = hash[state]
-
 
     state_value = float("inf")
     best_word = None
@@ -42,7 +39,7 @@ def compute_value(state, all_guesses, colouring_data, proccessUnique_list, share
     for index, action in enumerate(current_list):
         if depth == 0:
             progress = int((index / len(current_list)) * 100)
-            if progress % 10 == 0 and progress != last_printed_progress:
+            if progress % 20 == 0 and progress != last_printed_progress:
                 print(f"{multiprocessing.current_process().name:<10} | Progress: {progress}%")
                 last_printed_progress = progress
 
@@ -72,8 +69,8 @@ def compute_value(state, all_guesses, colouring_data, proccessUnique_list, share
     v_mem[(depth, state)] = (state_value, best_word)
     return state_value, best_word
 
-def process_task(state, all_guesses, colouring_data, processUnique_list, shared_state, hash, result_queue):
-    results = compute_value(state, all_guesses, colouring_data, processUnique_list, shared_state, hash)
+def process_task(state, all_guesses, colouring_data, filtered_split_actions, shared_state, hash, result_queue):
+    results = compute_value(state, all_guesses, colouring_data, filtered_split_actions, shared_state, hash)
     result_queue.put((multiprocessing.current_process().name, results))
 
 def set_cpu_affinity(pid, core_id):
@@ -92,7 +89,6 @@ if __name__ == "__main__":
     PROCESSES = min(10, os.cpu_count())
     DATA_DIRECTORY = os.path.join(os.path.dirname(__file__), '..', 'data')
     GUESSES_PATH = os.path.join(DATA_DIRECTORY, 'allowed_guesses.txt')
-    ANSWERS_PATH = os.path.join(DATA_DIRECTORY, 'allowed_answers.txt')
     PRECOMPUTE_PATH = os.path.join(DATA_DIRECTORY, 'precompute.json')
 
     manager = multiprocessing.Manager()
@@ -102,42 +98,37 @@ if __name__ == "__main__":
 
     colouring_data = load_colouring(PRECOMPUTE_PATH)
     all_guesses = load_words(GUESSES_PATH)
-    state = ('shelf', 'shell', 'slice', 'slide', 'slime', 'slope', 'smell', 'smile', 'spell', 'swell')
+    state = ('scene', 'scone', 'scope', 'score', 'seedy', 'segue', 'seize', 'sense', 'serif', 'serum', 'serve', 'sheik', 'shine', 'shire', 'shone', 'shore', 'shove', 'siege', 'sieve', 'since', 'singe', 'smoke', 'snide', 'snipe', 'snore', 'speck', 'spend', 'sperm', 'spice', 'spike', 'spine', 'spire', 'spoke', 'spore', 'surge', 'swine', 'swore')
 
-    processUnique_sublists = heuristic(all_guesses, state, colouring_data, PROCESSES)
+    filtered_split_actions = heuristic(all_guesses, state, colouring_data, PROCESSES)
 
-    for i in (processUnique_sublists):
-        print(len(i))
 
     manager = multiprocessing.Manager()
     result_queue = manager.Queue()
 
-    extra_process = multiprocessing.Process(
-        target=process_task,
-        args=(state, all_guesses, colouring_data, list(state), shared_state, hash, result_queue)
-    )
+  
 
     processes = [
         multiprocessing.Process(
             target=process_task,
-            args=(state, all_guesses, colouring_data, processUnique_sublists[i], shared_state, hash, result_queue)
+            args=(state, all_guesses, colouring_data, filtered_split_actions[i], shared_state, hash, result_queue)
         ) for i in range(PROCESSES)
     ]
 
-    # extra_process.start()
-    # set_cpu_affinity(extra_process.pid, 0)
+ 
     start_time = time.time()
-    i = -1
+    i = 0
     for p in processes:
         i+=1
         p.start()
         set_cpu_affinity(p.pid, i)
         
 
-    # extra_process.join()
     for p in processes:
         p.join()
     end_time = time.time()
+
+
     results = []
     print(f"\nRESULTS:")
     while not result_queue.empty():

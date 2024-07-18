6f42b714fbdc873ef397dfcded74a9fede5cdc1d
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2024-07-18 14:10:44 +0100

    change heuristic percentile and add an array splitting function

--- src/helpers.py
+++ src/helpers.py
@@ -1,10 +1,52 @@
-from typing import List, Dict
+import orjson
 import os
 import platform
-import orjson
 import numpy as np
 
-def get_tile_coloring(guess, solution):
+def clear_terminal():
+    current_os = platform.system()
+    if current_os == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def load_words(file_path):
+    with open(file_path, "r") as file_handle:
+        word_array = [word.strip() for word in file_handle]
+    return word_array
+
+def load_colouring():
+    # Create the full path to the precompute.json file
+    precompute_full_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'precompute.json'))
+
+    with open(precompute_full_path, "rb") as file:
+        data = orjson.loads(file.read())
+    return data
+
+def get_transition_info(state, action, colouring_data):
+    # Initialize an empty dictionary to store new states based on the action taken
+    new_state = {}
+    
+    # Iterate through each possible solution in the current state
+    for possible_solution in state:
+        # Get the colouring for the current action and possible solution from the colouring_data
+        colouring = colouring_data[action][possible_solution]
+        
+        # If the colouring is not already a key in new_state, add it with the current possible solution as its first value
+        if colouring not in new_state:
+            new_state[colouring] = [possible_solution]
+        else:
+            # If the colouring is already a key, append the current possible solution to the list of solutions
+            new_state[colouring].append(possible_solution)
+    
+    # Create a dictionary to store the transition information
+    # Keys are tuples of states, values are the the probability of transitioning into said state
+    transition_info = {tuple(states): (len(states) / len(state)) for states in new_state.values()}
+    
+    # Return the transition information dictionary
+    return transition_info
+
+def get_tile_colouring(guess, solution):
     result = ['-'] * len(guess)  # Start with all white
     solution_counts = {}
 
@@ -29,40 +71,11 @@ def get_tile_coloring(guess, solution):
                 result[i] = 'y'
                 solution_counts[char] -= 1
 
-    return ''.join(result)
-
-def get_transition_info(state, action, colouring_data):
-    new_state = {}
-    for word in state:
-        colouring = colouring_data[action][word]
-        if colouring not in new_state:
-            new_state[colouring] = [word]
-        else:
-            new_state[colouring].append(word)
-    transition_info = {tuple(states): (len(states) / len(state)) for states in new_state.values()}
-    return transition_info
-
-def clear_terminal():
-    current_os = platform.system()
-    if current_os == "Windows":
-        os.system('cls')
-    else:
-        os.system('clear')
-
-def load_words(file_path):
-    with open(file_path, "r") as file_handle:
-        word_array = [word.strip() for word in file_handle]
-    return word_array
-
-def load_colouring(file_path):
-    base_path = os.path.abspath(os.path.dirname(__file__))
-    precompute_full_path = os.path.join(base_path, file_path)
-    with open(precompute_full_path, "rb") as file:
-        data = orjson.loads(file.read())
-    return data
+    # return the tile colouring
+    return ''.join(result) 
 
 def heuristic(actions, state,colouring_data, processes_to_split=1):
-    percentile = 99.95 if processes_to_split != 1 else 99.9
+    percentile = 98.5
     transition_counts = {guess: len(get_transition_info(state, guess, colouring_data)) for guess in actions}
     lower_bound = np.percentile(list(transition_counts.values()), percentile)
     high_value_actions = [word for word, count in transition_counts.items() if count >= lower_bound]
@@ -72,3 +85,10 @@ def heuristic(actions, state,colouring_data, processes_to_split=1):
         return [list(map(str, sublist)) for sublist in split_arrays]
     else:
         return high_value_actions
+
+def split_arrays(array, processes_to_split):
+    if processes_to_split != 1:
+        split_arrays = np.array_split(array, processes_to_split)
+        return [list(map(str, sublist)) for sublist in split_arrays]
+    else:
+        return array
